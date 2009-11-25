214b2ced06418ee04f44ac248069b451b4bfca11
utada_phoenix@hotmail.com <utada_phoenix@hotmail.com@8387d62a-d070-11de-a306-d16691e8d855>
2009-11-25 16:39:41 +0000

    Jess: Semi complete events handler, contains events interface, two event classes and the events handler class which controls activation.

--- Prototype/PlayerAction.cs
+++ Prototype/PlayerAction.cs
@@ -20,46 +20,121 @@ namespace Graphics_Code_SO
 {
     interface EventsInterface
     {
-     //BoundingSphere sphere;
+        bool getStatus();//Jess:get whether event is active
+        int whichEvent();//used to determine
+      BoundingSphere GetBoundingSphere();
       void Activate();
     }
 
-    class CustomEvent : EventsInterface
+    //growing flower event
+    class GrowEvent : EventsInterface
     {
         BoundingSphere sphere;
-        //jess: Sets position and radius for event bounding sphere. :) 
-        public void SetBoundingSphere(int x,int y,int z,int r)
+        bool active;
+        int thisEvent;
+
+        //jess: Sets position and radius for event bounding sphere. Also set active to true/false :) 
+        public void SetEvent(int x,int y,int z,int r, bool act)
         {
-            //sphere.Center = Vector3(x,y,z);
-            sphere.Radius = r;
+          sphere.Center =new Vector3(x,y,z);//set position
+          sphere.Radius = r;//set radius
+          active = act;//set active bool
+          thisEvent = 1;//set event indicator
 
         }
 
         public void Activate()
         {
-            //translate box by something
+
+            //create box & platforms
+            //translate box so it grows up...while loop perhaps
+            //set active to false
             
         }
+
+       public BoundingSphere GetBoundingSphere()//return bounding sphere
+        {
+            return sphere;
+        }
+
+       public bool getStatus()//return whether active
+       {
+           return active;
+       }
+
+       public int whichEvent()//return event indicator
+       {
+           return thisEvent;
+       }
+    }
+
+    //Jess: running past & watering flowers event
+    class FlowerEvent : EventsInterface
+    {
+        BoundingSphere sphere;
+        bool active;
+        int thisEvent;
+
+        //jess: Sets position and radius for event bounding sphere. Also set active to true/false :) 
+        public void SetEvent(int x, int y, int z, int r, bool act)
+        {
+            sphere.Center = new Vector3(x, y, z);//set position
+            sphere.Radius = r;//set radius
+            active = act;//set active bool
+            thisEvent = 2;//set event indicator
+
+        }
+
+        public void Activate()
+        {
+
+            //animate model
+            //animate flower
+            //activate grey to colour change
+            //setactive to false
+
+        }
+
+        public BoundingSphere GetBoundingSphere()//return bounding sphere
+        {
+            return sphere;
+        }
+
+        public bool getStatus()//return whether active
+        {
+            return active;
+        }
+
+        public int whichEvent()//return event indicator
+        {
+            return thisEvent;
+        }
+    }
+
+
+
 
         //Jess: loops through list of events and checks for intersection with the player
         class EventsController
         {
 
-            public static List<CustomEvent> GameEvents;
-           public static List<CustomEvent>.Enumerator iterator;
+            public static List<EventsInterface> GameEvents;
+           public static List<EventsInterface>.Enumerator iterator;
 
-            public void CheckBounding(Player player)
+            public void CheckBounding(Player player, int anEvent)
             {
-            foreach( CustomEvent Ev in GameEvents)
+            foreach( EventsInterface Ev in GameEvents)
             {
-              //  if (player.bounding.intersect(Ev.sphere))
+                //if (Ev.whichEvent == anEvent)//only loop through specific type of event i.e. growevent =1 and flowerevent = 2
+                   //if (Ev.getStatus() == true)
+              //  if (player.bounding.intersect(Ev.GetBoundingSphere()))
                //     Ev.Activate();
-        }
+             }
             }
 
 
-        }
-    }
+         }
+    
 }
 //check if player is undisposed, check if player is close enough to nearest context sensitive object, check if object is 
 //ready for usage, 
