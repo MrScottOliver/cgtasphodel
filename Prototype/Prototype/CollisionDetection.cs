52c2d2b21d9476c677659bcd9b60916b99156de3
calibur_45@hotmail.co.uk <calibur_45@hotmail.co.uk@8387d62a-d070-11de-a306-d16691e8d855>
2010-02-04 15:08:21 +0000

    This is the latest build which includes a camera which tracks the player's position and stuff.

---
+++ Prototype/Prototype/CollisionDetection.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Prototype
+{
+    static class CollisionDetectionPlane
+    {
+        static List<PlaneObject> BObjects;
+
+        static CollisionDetectionPlane()
+        {
+            BObjects = new List<PlaneObject>();
+        }
+
+        static public void AddPlane(Vector3 x, Vector3 y, Vector3 z, float min, float max)
+        {
+            BObjects.Add(new PlaneObject(x,y,z,min,max));
+        }
+
+        static public void Compare(ref Player playerObject)
+        {
+            int result;
+
+            foreach (PlaneObject planeX in BObjects)
+            {
+                if (playerObject.position.X > planeX.MinX && playerObject.position.X < planeX.MaxX)
+                {
+                    playerObject.boundingsphere.Intersects(ref planeX.p, out planeX.next);
+                    result = planeX.Position();
+                    if (result == 1)
+                    {
+                        playerObject.velocity = Vector3.Zero;
+                        playerObject.position += new Vector3(0, 0.001f, 0);
+                    }
+                    if (result == -1)
+                    {
+                        playerObject.velocity.Normalize();
+                        playerObject.velocity *= -1;
+                    }
+                    if (result == 0)
+                    {
+                        playerObject.velocity = Vector3.Zero;
+                    }
+                }
+            }
+        }
+    }
+
+    static class CollisionDetectionBox
+    {
+        static List<BoundingBox> Blist;
+
+        static CollisionDetectionBox()
+        {
+            Blist = new List<BoundingBox>();
+        }
+
+        public static void AddBox(Vector3 min, Vector3 max)
+        {
+            Blist.Add(new BoundingBox(min, max));
+        }
+
+        public static void Compare(ref Player playerObject)
+        {
+            foreach (BoundingBox box in Blist)
+            {
+                if (playerObject.boundingsphere.Intersects(box))
+                {
+                    playerObject.velocity = Vector3.Zero;
+                }
+            }
+        }
+    }
+}
