f9a7acb5a78cc8d8cf16e9a770ee36bb9f2a14c7
piepork@hotmail.com <piepork@hotmail.com@8387d62a-d070-11de-a306-d16691e8d855>
2010-04-26 11:43:32 +0000

    82 didnt work, retry

---
+++ branches/Main Development/Prototype/Prototype/PhysicsActor.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Prototype
+{
+    // Manages a PhysicsObject and allows the engine to work
+    // with it
+    public class PhysicsActor
+    {
+        // The object we are managing
+        public IPhysicsObject PhysicsObject;
+
+        // Override the position of the base class to that
+        // of the physics object
+        public Vector3 Position
+        {
+            get { return PhysicsObject.Position; }
+            set
+            {
+                if (PhysicsObject != null)
+                    PhysicsObject.Position = value;
+            }
+        }
+
+        // Override the rotation of the base class to that
+        // of the physics object
+        public Vector3 Rotation
+        {
+            get { return PhysicsObject.Rotation; }
+            set
+            {
+                if (PhysicsObject != null)
+                    PhysicsObject.Rotation = value;
+            }
+        }
+
+        // Override the BoundingBox of the base class to that
+        // of the physics object
+        public BoundingBox BoundingBox
+        {
+            get { return PhysicsObject.BoundingBox; }
+        }
+
+        // Constructors
+
+        public PhysicsActor(Model Model, IPhysicsObject PhysicsObject)
+        {
+            this.PhysicsObject = PhysicsObject;
+        }
+
+
+        // Override DisableComponent so we can remove the physics
+        // object as well
+        public void DisableComponent()
+        {
+            this.PhysicsObject.DisableComponent();
+        }
+    }
+}
